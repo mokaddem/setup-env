376ed89877bc37d2300da362048dde37739ae6c5
Sami Mokaddem <mokaddem.sami@gmail.com>
2018-09-27 14:20:24 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -24,7 +24,7 @@ mkdir -p ~/.vim
  fi
 
  ##Installing powerline fonts
- git clone git@github.com:powerline/fonts.git
+ git clone https://github.com/powerline/fonts.git
  cd fonts/
  ./install.sh
  cd ..
